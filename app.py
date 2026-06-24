05c56d59dce57d9cd535836082942496b1573e5c
reddykumarikandikatla7-web <reddykumarikandikatla7@gmail.com>
2026-06-24 16:38:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,27 @@ vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
 st.title("Spam Email Classifier")
 
-# User input
+# Manual text input
 message = st.text_area("Enter message")
 
-# Button
-if st.button("Check"):
-    message_vector = vectorizer.transform([message])
-    result = model.predict(message_vector)
+# File upload option
+uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
 
-    if result[0] == "spam":
-        st.error("This is Spam")
+# Read uploaded file
+if uploaded_file is not None:
+    message = uploaded_file.read().decode("utf-8")
+    st.write("Uploaded Message:")
+    st.write(message)
+
+# Check button
+if st.button("Check"):
+    if message:
+        message_vector = vectorizer.transform([message])
+        result = model.predict(message_vector)
+
+        if result[0] == "spam":
+            st.error("This is Spam")
+        else:
+            st.success("This is Normal")
     else:
-        st.success("This is Normal")
+        st.warning("Please enter or upload a message")
